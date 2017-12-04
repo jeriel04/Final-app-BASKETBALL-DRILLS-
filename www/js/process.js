b1d9d2f7d61c89bcf26417e720e4a0508fa24b3b
jeriel04 <jerielreyes24@gmail.com>
2017-12-04 09:39:12 +0800

    lastApp

---
+++ www/js/process.js
@@ -0,0 +1,234 @@
+let process = {
+  menu:function(){
+    let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Basketball Drills</div>
+              </div>
+            </div>
+
+    <div className="list-group">
+      <ul>
+        <li className="list-group-title"></li>
+        <li><a href='#' onClick={process.Dribbling}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Dribbling</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.Passing}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Passing</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.Shooting}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Shooting</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.Rebounding}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Rebounding</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.Layup}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Layup</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+        <li><a href='#' onClick={process.Jumpball}>
+          <div className="item-content">
+            <div className="item-inner">
+              <div className="item-title">Jumpball</div>
+                  <div className="item-after">Details</div>
+            </div>
+          </div></a>
+        </li>
+      </ul>
+    </div>
+
+</div>
+</div>
+</div>
+</div>
+</div>;
+    ReactDOM.render(content,document.getElementById('root'));
+  },
+  Alicorn:function(){
+    let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">World of Myth</div>
+              </div>
+            </div>
+            <center><img src={"img/imp.jpg"}/>
+            <p>asdajslfkjsldkjadlk jalkjlksjdlkaj lkj lakjskldj </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  Dribbling:function(){
+         let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Dribbling</div>
+              </div>
+            </div>
+            <center><img src={"img/b1.jpg"} width={"100%"}/>
+            <p>In basketball, dribbling is the legal method of advancing the ball by oneself, as opposed to passing it to another player or shooting for the basket. It consists of bouncing the ball on the floor continuously with one hand while walking or running down the court. </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  Passing:function(){
+         let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Passing</div>
+              </div>
+            </div>
+            <center><img src={"img/b.jpg"} width={"100%"}/>
+            <p>Passing is a common technique in sports that use balls and pucks. ... Passing in basketball has been defined as "The deliberate attempt to move a live ball between two teammates", a definition which might equally apply across other sports equally well, albeit with a change to the item being passed where appropriate. </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  Shooting:function(){
+  let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Shooting</div>
+              </div>
+            </div>
+            <center><img src={"img/b2.jpg"} width={"100%"}/>
+            <p>A basketball shot that bounces off of the backboard before passing through the hoop. dunk, dunk shot, stuff shot. a basketball shot in which the basketball is propelled downward into the basket. finger-roll. a basketball shot that rolls off the tips of the fingers into the basket. </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+    Rebounding:function(){
+     let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Rebounding</div>
+              </div>
+            </div>
+            <center><img src={"img/b3.jpg"} width={"100%"}/>
+            <p>In basketball, a rebound, colloquially referred to as a board, is a statistic awarded to a player who retrieves the ball after a missed field goal or free throw. Rebounds are also given to a player who tips in a missed shot on his team's offensive end.
+ </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  Layup:function(){
+         let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Layup</div>
+              </div>
+            </div>
+            <center><img src={"img/b4.jpg"} width={"100%"}/>
+            <p>A layup in basketball is a two-point shot attempt made by leaping from below, laying the ball up near the basket, and using one hand to bounce it off the backboard and into the basket. The motion and one-handed reach distinguish it from a jump shot. </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  Jumpball:function(){
+         let content = <div className="page">
+    <div className="page-content infinite-scroll infinite-scroll-top">
+<div className="page-content contacts-content">
+  <div className="list-block contacts-block">
+    <div className="list-group">
+     <div className="navbar">
+              <div className="navbar-inner">
+                <div className="center">Jumpball</div>
+              </div>
+            </div>
+            <center><img src={"img/b5.jpg"} width={"100%"}/>
+            <p>A jump ball is a method used to begin or resume play in basketball. It is similar to a face-off in ice hockey or field lacrosse, or ball-up in Australian rules football. Two opposing players attempt to gain control of the ball after it is tossed up into the air in between them by an official.. </p></center>
+            <br/><a href='#' onClick={process.menu}>back</a>
+</div>
+</div>
+</div>
+</div>
+</div>;
+      ReactDOM.render(content,document.getElementById('root'));
+  },
+  loading:function(){
+    let content = <div>
+          Loading...
+        </div>;
+    ReactDOM.render(content,document.getElementById('root'));
+  }
+}
+
+process.loading();
+setTimeout(function(){
+  process.menu();
+},3000);
